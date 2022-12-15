e53833f8d0ab2c0056d4857fb55a6b84fbd990ea
Khalil Mubarak <khalil.f.mubarak@gmail.com>
2022-12-15 12:38:49 -0500

    Completed wordsearch.js

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -35,4 +35,44 @@ describe("#wordSearch()", function() {
 
     assert.isTrue(result);
   });
+
+  it("should return true if the word is present vertically", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'R', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'A', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'N', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'K', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'FRANK')
+
+    assert.isTrue(result);
+  });
+
+  it("should return false if the length of letters is 0", function() {
+    const result = wordSearch([], 'FRANK')
+
+    assert.isFalse(result);
+  });
+
+  it("should return false if letters include an empty array", function() {
+    const result = wordSearch([[]], 'FRANK')
+
+    assert.isFalse(result);
+  });
+
+  it("should return false if letters include anything but an array", function() {
+    const result = wordSearch([["B2",2,3], ["2", 1, "b"], [1,2,4]], 'FRANK')
+    assert.isFalse(result);
+  });
+
+  it("should return false if the string is not a letter", function() {
+    const result = wordSearch([["A","B", "C"], ["2", "D", "E"]], 'FRANK')
+    assert.isFalse(result);
+  });
 });
+
+
